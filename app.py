91b2e87f0a3f927aee182806c4bfe63407479164
Vrutik-0 <238077214+Vrutik-0@users.noreply.github.com>
2026-02-01 20:58:51 +0530

    DashBoard Routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for ,session
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import get_db_connection
 import os
@@ -10,6 +10,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 def home():
     return redirect(url_for("login"))
 
+#Signup
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -38,6 +39,7 @@ def signup():
 
     return render_template("signup.html")
 
+#Login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -46,17 +48,53 @@ def login():
 
         conn = get_db_connection()
         cur = conn.cursor()
-        cur.execute("SELECT id, password, role FROM users WHERE email = %s", (email,))
+        cur.execute("SELECT id, password, role, name FROM users WHERE email = %s", (email,))
         user = cur.fetchone()
         cur.close()
         conn.close()
 
         if user and check_password_hash(user[1], password):
-            return f"Login successful! Role: {user[2]}"
-        else:
-            return "Invalid credentials"
+            session["user_id"] = user[0]
+            session["role"] = user[2]
+            session["name"] = user[3]
+
+            if user[2] == "admin":
+                return redirect(url_for("adminBoard"))
+            elif user[2] == "camp_manager":
+                return redirect(url_for("campBoard"))
+            elif user[2] == "driver":
+                return redirect(url_for("driverBoard"))
+            else:
+                return "Invalid role"
+
+        return "Invalid credentials"
 
     return render_template("login.html")
+
+@app.route("/admin/dashboard")
+def adminBoard():
+    if session.get("role") != "admin":
+        return redirect(url_for("login"))
+    return render_template("dashboard/admin.html")
+
+
+@app.route("/camp/dashboard")
+def campBoard():
+    if session.get("role") != "camp_manager":
+        return redirect(url_for("login"))
+    return render_template("dashboard/camp_manager.html")
+
+
+@app.route("/driver/dashboard")
+def driverBoard():
+    if session.get("role") != "driver":
+        return redirect(url_for("login"))
+    return render_template("dashboard/driver.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
